d64252e4263b19b70ba848ad6e94deb3aee9c869
Bryan Brouwer <bryanbrouwerus@gmail.com>
2025-07-27 13:34:19 +0200

    add: score and plant instance managers

---
+++ Assets/Scripts/Plant/PlantInstanceManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+
+namespace Plant
+{
+    public class PlantInstanceManager
+    {
+        private static PlantInstanceManager _instance;
+
+        public static PlantInstanceManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new PlantInstanceManager();
+                return _instance;
+            }
+        }
+        
+        private readonly Dictionary<PlantType, List<PlantInstance>> _plantInstances = new Dictionary<PlantType, List<PlantInstance>>();
+        
+        private PlantInstanceManager()
+        {
+        }
+
+        public void AddPlantInstance(PlantInstance plantInstance)
+        {
+            if (!_plantInstances.ContainsKey(plantInstance.seedData.plantType))
+                _plantInstances.Add(plantInstance.seedData.plantType, new List<PlantInstance>());
+            _plantInstances[plantInstance.seedData.plantType].Add(plantInstance);
+        }
+        
+        public void RemovePlantInstance(PlantInstance plantInstance)
+        {
+            if (_plantInstances.TryGetValue(plantInstance.seedData.plantType, out var instance))
+                instance.Remove(plantInstance);
+        }
+        
+        public List<PlantInstance> GetPlantInstances(PlantType plantType)
+        {
+            return _plantInstances[plantType];
+        }
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class ScoreManager
+{
+    private static ScoreManager _instance;
+
+    public static ScoreManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+                _instance = new ScoreManager();
+            return _instance;
+        }
+    }
+        
+    private int _score;
+    private event Action<int> OnScoreChanged;
+        
+    private ScoreManager()
+    {
+    }
+        
+    public void AddScore(int score)
+    {
+        _score += score;
+        OnScoreChanged?.Invoke(_score);
+    }
+        
+    public int GetScore()
+    {
+        return _score;
+    }
+        
+    public void ResetScore()
+    {
+        _score = 0;
+        OnScoreChanged?.Invoke(_score);
+    }
+}
