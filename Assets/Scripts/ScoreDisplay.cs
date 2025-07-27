b74f68e4b07b44985f9da2eff2ad60dce9eb9ac3
Bryan Brouwer <bryanbrouwerus@gmail.com>
2025-07-27 19:51:51 +0200

    add: score display UI

--- Assets/Scripts/ScoreDisplay.cs
+++ Assets/Scripts/ScoreDisplay.cs
@@ -1,10 +1,15 @@
-﻿using UnityEngine;
+﻿using TMPro;
+using UnityEngine;
 
 public class ScoreDisplay : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI scoreText;
+    [SerializeField] private string scoreTextPrefix;
+    
     public void UpdateScore(int score)
     {
         Debug.Log(score);
+        scoreText.text = scoreTextPrefix + score;
     }
         
     private void OnEnable()
